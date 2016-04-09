1030a1e3fde02eab66f4f0617eca6afc139df8bc
jack1582 <xhuan@vip.qq.com>
2016-04-10 00:35:27 +0800

    add a sql file to register the function in mysql

---
+++ src/reg.sql
@@ -0,0 +1,4 @@
+create function  http_get returns string soname 'mysql-udf-http.so';
+create function  http_post returns string soname 'mysql-udf-http.so';
+create function  http_put returns string soname 'mysql-udf-http.so';
+create function  http_delete returns string soname 'mysql-udf-http.so';
